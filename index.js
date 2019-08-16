5b7dd596237529fee6752a53a1519d9f1813223f
dada-tech <daviddadaa@hotmail.com>
2019-08-16 07:50:47 -0600

    mailing works but goes to spam

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
 const express = require('express');
 const Joi = require('@hapi/joi'); // class returned
-const app = express();
+require('dotenv').config();
+
+// SendGrid
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 // use JSON parse in body. middleware
+const app = express();
 app.use(express.json());
 
 // in terminal: "source .env"
 const port = process.env.PORT || 3000;
-
 
 // GET
 app.get('/',(req,res) => {
     res.send("Hello World");
 });
 
-
 app.get('/api/courses/:id',(req,res) => {
     res.send([req.params,req.query]);
 });
-
 
 // POST
 app.post('/api/mail',(req,res) => {
@@ -39,12 +41,28 @@ app.post('/api/mail',(req,res) => {
         from: 'david-dada-portfolio@dadadavid.com',
         subject: req.body.subject.toString(),
         text: req.body.text,
-        html: '<strong>sent from Portfolio</strong>',
+        html: '<strong>sent from Node Server</strong>',
     };
+
+    sgMail
+        .send(msg)
+        .then(() => {
+            //Celebrate
+            // console.log('Email Sent!');
+        })
+        .catch(error => {
+            //Log friendly error
+            console.error(error.toString());
+
+            //Extract error msg
+            const {message, code, response} = error;
+
+            //Extract response msg
+            const {headers, body} = response;
+        });
 
     res.send("email SENT");
 });
-
 
 app.listen(port,() => console.log(`Listening on port ${port}...`));
 
